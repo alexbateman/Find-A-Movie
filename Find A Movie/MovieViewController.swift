e80a4dafab4e9e36b14e50abc8a2ec50b24c5611
Alex Bateman <bateman311@gmail.com>
2016-05-23 17:29:04 -0600

    All views and view controllers set up

---
+++ Find A Movie/MovieViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MovieViewController.swift
+//  Find A Movie
+//
+//  Created by Alexander on 5/23/16.
+//  Copyright © 2016 Alex Bateman. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MovieViewController: UIViewController {
+    
+    
+}
