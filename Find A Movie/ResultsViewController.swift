9af9db11d96d2e44e59ff085234707db3474f80f
Alex Bateman <bateman311@gmail.com>
2016-05-23 17:21:24 -0600

    View hierarchy is done

---
+++ Find A Movie/ResultsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ResultsViewController.swift
+//  Find A Movie
+//
+//  Created by Alexander on 5/23/16.
+//  Copyright © 2016 Alex Bateman. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ResultsViewController: UITableViewController {
+    
+    
+    
+}
---
+++ Find A Movie/SearchViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SearchViewController.swift
+//  Find A Movie
+//
+//  Created by Alexander on 5/23/16.
+//  Copyright © 2016 Alex Bateman. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+    
+    
+}
